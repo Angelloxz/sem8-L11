22dfcc98153517ed4aad64b1f951eff9b0a0ee17
Angelloxz <138254018+Angelloxz@users.noreply.github.com>
2023-07-23 21:09:10 +0000

    Avance3

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,21 +1,16 @@
 package com.example.demo;
 
-import javax.management.ConstructorParameters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.jdbc.core.JdbcTemplate;
-import java.util.List;
-import java.util.Map;
+
 import java.lang.String;
-import java.lang.Object;
+
 
 @Controller // This means that this class is a Controller
 @RequestMapping(path="/")
